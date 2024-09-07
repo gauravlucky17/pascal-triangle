16d555670e84d1666d2a34cff7f890f9164dd8b3
gauravlucky17 <luckygaurav95@gmail.com>
2024-09-07 13:36:28 +0530

    commit

---
+++ pascal triangle/Controllers/FebonnaciController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pascal_triangle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FebonnaciController : ControllerBase
+    {
+        [HttpGet("series")]
+        public IActionResult GetFibonacciSeries([FromQuery] int terms)
+        {
+            if (terms <= 0)
+            { return BadRequest("Number of terms must be a positive integer."); }
+            var series = GenerateFibonacciSeries(terms);
+            return Ok(series);
+        }
+        private List<int> GenerateFibonacciSeries(int terms)
+        {
+            var series = new List<int>();
+            if (terms >= 1)
+            { series.Add(0); }
+            if (terms >= 2)
+            { series.Add(1); }
+            int first = 0, second = 1, next;
+            for (int i = 2; i < terms; i++)
+            {
+                next = first + second;
+                series.Add(next);
+                first = second;
+                second = next;
+            }
+            return series;
+        }
+
+    }
+}
---
+++ pascal triangle/Controllers/RemoveDuplicateArrayController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pascal_triangle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RemoveDuplicateArrayController : ControllerBase
+    {
+        [HttpPost("removeduplicates")]
+        public IActionResult RemoveDuplicates([FromBody] int[] array)
+        {
+            if (array == null || array.Length == 0)
+            { return BadRequest("Array cannot be null or empty."); }
+            int[] arrayWithoutDuplicates = RemoveDuplicatesFromArray(array);
+            return Ok(arrayWithoutDuplicates);
+        }
+        private int[] RemoveDuplicatesFromArray(int[] array)
+        {
+            int[] tempArray = new int[array.Length];
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isDuplicate = false;
+                for (int j = 0; j < count; j++)
+                {
+                    if (array[i] == tempArray[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate)
+                {
+                    tempArray[count] = array[i];
+                    count++;
+                }
+            }
+            int[] resultArray = new int[count];
+            Array.Copy(tempArray, resultArray, count);
+            return resultArray;
+
+        }
+    }
+}
