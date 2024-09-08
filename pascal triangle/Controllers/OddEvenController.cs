42f7370816918fc3b635f5bb3c844af086151728
gauravlucky17 <luckygaurav95@gmail.com>
2024-09-08 18:36:42 +0530

    commit

---
+++ pascal triangle/Controllers/OddEvenController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pascal_triangle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OddEvenController : ControllerBase
+    {
+        [HttpPost("check")]
+        public IActionResult CheckOddEven([FromBody] int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            { return BadRequest("Array cannot be null or empty."); }
+            var results = new List<string>();
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0)
+                { results.Add($"{number} is even"); }
+                else
+                { results.Add($"{number} is odd"); }
+            }
+            return Ok(results);
+        }
+
+    }
+}
---
+++ pascal triangle/Controllers/ReverseStringController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pascal_triangle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReverseStringController : ControllerBase
+    {
+        [HttpGet("reverse")]
+        public IActionResult ReverseString([FromQuery] string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return BadRequest("Input string cannot be null or empty.");
+            }
+
+            string reversedString = Reverse(input);
+            return Ok(reversedString);
+        }
+
+        // Helper method to reverse a string
+        private string Reverse(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
